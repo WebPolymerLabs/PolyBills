aba018d1cf88f1d02254ddd1fb5125e3416e4c13
Vinicius Patrinhani <patrinhani@ciandt.com>
2017-12-26 11:00:22 -0200

    updt

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,2 @@
 /// <reference path="globals/es6-promise/index.d.ts" />
-/// <reference path="globals/gulp-load-plugins/index.d.ts" />
-/// <reference path="globals/localforage/index.d.ts" />
 /// <reference path="globals/moment/index.d.ts" />
